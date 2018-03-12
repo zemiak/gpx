f2f86f986b06ef9d358989265beb8161cf2b726f
zemiak <zemiak@gmail.com>
2018-03-12 21:47:29 +0100

    cmd line processing

--- src/main.go
+++ src/main.go
@@ -1,7 +1,31 @@
 package main
 
+import "os"
 import "fmt"
+import "flag"
+import "encoding/xml"
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+    args := os.Args[1:]
+    
+    if len(args) < 1 {
+    	fmt.Printf("Usage: gpx [-o outputFileName] inputFileName\n")
+        os.Exit(10)
+    }
+
+    if len(flag.Args()) > 0 {
+    	inputFileName = flag.Args()[0]
+    	inputFile, err := os.Open(inputFileName)
+    	check(err)
+    } else {
+    	inputFile = os.Stdin
+    }
+
+    res := readAndProcess(inputFile)
 }
